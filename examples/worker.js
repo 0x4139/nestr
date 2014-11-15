f2b16680436c12b583b4f44731e23b5898d10811
Vali Malinoiu <0x4139@gmail.com>
2014-11-15 20:11:34 +0200

    restarts worker in case it dies

--- examples/worker.js
+++ examples/worker.js
@@ -1,2 +1,5 @@
 var port = process.argv[2]; //AFIAK elements 0 and 1 are already populated with env info
-console.log('Worker on Port:',port);
+
+setInterval(function(){
+    console.log('Worker on Port:',port);
+},3000);
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var master = require('child_process');
-var os 	   = require('os');
+var os = require('os');
 
-function nestr (script,opt) {
-	var opts=opt || {};
-	this.script=script;
-	this.workers = opts.workers || os.cpus().length;
-	this.port = opts.startingPort || 3000;
-	this.addrs=[];
+function Nestr(script, opt) {
+    var opts = opt || {};
+    this.script = script;
+    this.workers = opts.workers || os.cpus().length;
+    this.port = opts.startingPort || 3000;
+    this.addrs = [];
 }
 
-nestr.prototype.start = function() {
-	console.log('Starting ',this.script,' on ',this.workers,' workers');
-	for (var i = 0; i < this.workers; i++) {
-		var port = this.port+i;
-		master.fork(this.script,[port]);
-		this.addrs.push({
-			host:'127.0.0.1',
-			port:port
-		})
-	}
+Nestr.prototype.start = function () {
+    console.log('[Nestr] starting ', this.script, ' on ', this.workers, ' workers');
+    for (var i = 0; i < this.workers; i++) {
+        var port = this.port + i;
+        this._startWorker(port);
+        this.addrs.push({
+            host: '127.0.0.1',
+            port: port
+        })
+    }
 };
 
-module.exports = nestr
+Nestr.prototype._startWorker= function(port){
+    var that=this;
+    var worker = master.fork(this.script, [port]);
+    worker.on('exit',function(){
+        console.log('[Nestr] restarting worker on port:',port);
+        that._startWorker(port);
+    });
+};
+
+module.exports = Nestr;
